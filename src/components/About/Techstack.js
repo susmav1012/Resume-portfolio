685f8948c734a8a3a7eb95874356b4be06fb5576
Susma V <106637847+susmav1012@users.noreply.github.com>
2023-01-24 22:04:35 +0530

    Add files via upload

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { CgCPlusPlus } from "react-icons/cg";
+import {
+  DiJavascript1,
+  DiReact,
+  DiPython,
+  DiCss3,
+  DiDjango,
+} from "react-icons/di";
+import {
+  SiHtml5,
+  SiAngular,
+  SiAngularjs,
+} from "react-icons/si";
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <CgCPlusPlus />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiPython />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAngularjs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAngular />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiDjango />
+      </Col>     
+    </Row>
+  );
+}
+export default Techstack;
